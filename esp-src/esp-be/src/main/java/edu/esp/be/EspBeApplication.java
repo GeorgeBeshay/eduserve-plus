b125f210f2247ab65c5ea5fa840faa476b4dba1a
George Beshay <georgebeshay101@gmail.com>
2023-11-14 15:31:26 +0200

    Spring Boot Application Created (Build Automation Tool Used: Gradle)

---
+++ esp-src/esp-be/src/main/java/edu/esp/be/EspBeApplication.java
@@ -0,0 +1,14 @@
+package edu.esp.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspBeApplication.class, args);
+		System.out.println("Working fine ..");
+	}
+
+}
