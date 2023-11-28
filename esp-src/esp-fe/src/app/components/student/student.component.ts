dcd6b99de3c438e9b2537df8f5b0529bbb108c0f
MariamAziz0 <ma27523@gmail.com>
2023-11-28 22:15:39 +0200

    Student sign in form in the front

--- esp-src/esp-fe/src/app/components/student/student.component.ts
+++ esp-src/esp-fe/src/app/components/student/student.component.ts
@@ -29,6 +29,15 @@ export class StudentComponent implements OnInit{
   }
 
   onSubmit() {
+    if (this.signInForm.valid) {
+      const id = this.signInForm.value.id;
+      const password = this.signInForm.value.password;
+
+      // Placeholder: Simulate authentication logic
+      console.log('Signing in with ID:', id, 'and password:', password);
+
+      // call API
+    }
   }
 
   onSignUp() {
