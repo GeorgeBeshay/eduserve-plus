64ec5dbbb3731c4ef7445a707af9ffddf565a41e
George Beshay <georgebeshay101@gmail.com>
2023-11-29 12:02:23 +0200

    Implemented DAOs Inserts

--- esp-src/esp-be/src/main/java/edu/esp/database/doas/AdminDAO.java
+++ esp-src/esp-be/src/main/java/edu/esp/database/doas/AdminDAO.java
@@ -1,11 +1,30 @@
 package edu.esp.database.doas;
 
+import edu.esp.system_entities.system_users.Admin;
+import edu.esp.system_entities.system_users.Instructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 public class AdminDAO {
     private final JdbcTemplate jdbcTemplate;
     public AdminDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean createAdmin(Admin newAdmin) {
+        try {
+            SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                    .withTableName("sys_admin");
+
+            BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(newAdmin);
+            int rowsAffected = jdbcInsert.execute(parameterSource);
+
+            return rowsAffected > 0;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false; // Return a meaningful response indicating failure
+        }
     }
 
 }
--- esp-src/esp-be/src/main/java/edu/esp/database/doas/InstructorDAO.java
+++ esp-src/esp-be/src/main/java/edu/esp/database/doas/InstructorDAO.java
@@ -1,11 +1,30 @@
 package edu.esp.database.doas;
 
+import edu.esp.system_entities.system_users.Instructor;
+import edu.esp.system_entities.system_users.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 public class InstructorDAO {
     private final JdbcTemplate jdbcTemplate;
 
     public InstructorDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public boolean createInstructor(Instructor newInstructor) {
+        try {
+            SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                    .withTableName("instructor");
+
+            BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(newInstructor);
+            int rowsAffected = jdbcInsert.execute(parameterSource);
+
+            return rowsAffected > 0;
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+            return false; // Return a meaningful response indicating failure
+        }
     }
 }
