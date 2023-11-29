51a58f9c8cd620121ecdfa86ff3c3102c49edd26
MariamAziz0 <ma27523@gmail.com>
2023-11-29 09:27:05 +0200

    Adding student sign up form

--- esp-src/esp-fe/src/app/components/student/student.component.ts
+++ esp-src/esp-fe/src/app/components/student/student.component.ts
@@ -20,7 +20,15 @@ export class StudentComponent implements OnInit{
       id: ['', Validators.required],
       password: ['', Validators.required],
       newPassword: ['', Validators.required],
-      confirmNewPassword: ['', Validators.required]
+      confirmNewPassword: ['', Validators.required],
+      fullName: ['', Validators.required],
+      ssn: ['', Validators.required],
+      dateOfBirth: ['', Validators.required],
+      address: ['', Validators.required],
+      phone: ['', Validators.required],
+      landline: ['', Validators.required],
+      email: ['', Validators.required],
+      gender: ['', Validators.required]
     });
   }
 
@@ -41,6 +49,33 @@ export class StudentComponent implements OnInit{
   }
 
   onSignUp() {
+    if (this.signUpForm.valid) {
+      const id = this.signUpForm.value.id
+      const password = this.signUpForm.value.password;
+      const newPassword = this.signUpForm.value.newPassword;
+      const confirmNewPassword = this.signUpForm.value.confirmNewPassword;
+      const fullName = this.signUpForm.value.fullName
+      const ssn = this.signUpForm.value.ssn
+      const dateOfBirth = this.signUpForm.value.dateOfBirth
+      const address = this.signUpForm.value.address
+      const phone = this.signUpForm.value.phone
+      const landline = this.signUpForm.value.landline
+      const email = this.signUpForm.value.email
+      const gender = this.signUpForm.value.gender
+
+      // Placeholder: Simulate authentication logic
+      console.log('Signing in with ID:', id, ', password:', password,
+        ', new password:', newPassword,
+        ', confirm password: ', confirmNewPassword,
+        ', fullName: ', fullName,
+        ', snn: ', ssn,
+        ', dateOfBirth: ', dateOfBirth,
+        ', address: ', address,
+        ', phone: ', phone,
+        ', landline: ', landline,
+        ', email: ', email,
+        ', gender: ', gender);
+    }
   }
 
   onTabChanged(event: number) {
