1126d3bc876784fe8c4afffeb430d4c098668527
marshelino-maged <maromaged169@gmail.com>
2023-12-08 06:51:20 +0200

    implement function to add new course with its prerequisite in DAO

--- esp-src/esp-be/src/main/java/edu/esp/database/daos/AdminDAO.java
+++ esp-src/esp-be/src/main/java/edu/esp/database/daos/AdminDAO.java
@@ -1,11 +1,18 @@
 package edu.esp.database.daos;
 
 import edu.esp.system_entities.system_users.Admin;
+import edu.esp.system_entities.system_users.Course;
+import edu.esp.utilities.Logger;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AdminDAO {
@@ -76,5 +83,52 @@ public class AdminDAO {
         }
     }
 
+    @Transactional
+    public boolean addNewCourse(Course newCourse, List<String> preqId) {
 
+        try {
+
+            insertIntoCourseTable(newCourse);
+            if(preqId!=null && preqId.size()>0) {
+                batchInsertIntoCoursePrereqTable(newCourse.getCourseCode(), preqId);
+            }
+            return true;
+
+        } catch (Exception e){
+
+            return false;
+        }
+    }
+
+    @Transactional
+    private void insertIntoCourseTable(Course newCourse){
+
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate).withTableName("course");
+
+        BeanPropertySqlParameterSource parameterSource = new BeanPropertySqlParameterSource(newCourse);
+
+        simpleJdbcInsert.execute(parameterSource);
+
+    }
+
+    @Transactional
+    private void batchInsertIntoCoursePrereqTable(String courseCode, List<String> prereq){
+
+        String insertQuery = "INSERT INTO course_prereq (course_code, preq_id) VALUES (?, ?)";
+
+        jdbcTemplate.batchUpdate(insertQuery, new BatchPreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement preparedStatement, int i) throws SQLException {
+                String prereqElement = prereq.get(i);
+                preparedStatement.setString(1, courseCode);
+                preparedStatement.setString(2, prereqElement);
+            }
+
+            @Override
+            public int getBatchSize() {
+                return prereq.size();
+            }
+        });
+
+    }
 }
