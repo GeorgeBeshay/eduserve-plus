6677ca4f1d9bdad6abe0476bc08ca6de3a79029c
Patrick Georges <patrick.georges18@gmail.com>
2023-12-08 19:46:40 +0200

    Transferred the trigger to a new file.

--- esp-src/esp-be/src/main/java/edu/esp/database/SQL/DDL.sql
+++ esp-src/esp-be/src/main/java/edu/esp/database/SQL/DDL.sql
@@ -179,17 +179,5 @@ CREATE TABLE ticket (
 );
 
 ALTER TABLE department ADD CONSTRAINT foreign_dep_inst foreign key (head_instructor_id) REFERENCES instructor(instructor_id)
---added a trigger so that whenever a new instructor signs up, his record gets deleted from the unregistered_instructor table
-CREATE TRIGGER DeleteCorrespondingTuple
-ON instructor
-AFTER INSERT
-AS
-BEGIN
-    SET NOCOUNT ON;
 
-    -- Assuming that 'ID' is the common attribute between the two tables
-    -- You may need to adjust the column names based on your actual schema
-    DELETE FROM unregistered_instructor
-    WHERE instructor_id IN (SELECT instructor_id FROM INSERTED);
-END;
 
