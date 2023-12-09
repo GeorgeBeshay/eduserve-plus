e7a7dc98cb9cf3f4554ced406fda199e6263a5a0
MariamAziz0 <ma27523@gmail.com>
2023-12-09 13:18:45 +0200

    changes

--- esp-src/esp-fe/src/app/System Entities/Student.ts
+++ esp-src/esp-fe/src/app/System Entities/Student.ts
@@ -1,31 +1,29 @@
 export class Student{
 
   studentId: string
-  studentPwHash: string
   departmentId: string
   studentLevel: string
   gpa: string
   studentName: string
   ssn: string
-  birthDate: string
+  bdate: string
   studentAddress: string
   phone: string
   landline: string
   gender: string
   email: string
 
-  constructor(studentId: string, studentPwHash: string, departmentId: string, studentLevel: string, gpa: string,
-              studentName: string, ssn: string, birthDate: string, studentAddress: string, phone: string,
+  constructor(studentId: string, departmentId: string, studentLevel: string, gpa: string,
+              studentName: string, ssn: string, bdate: string, studentAddress: string, phone: string,
               landline: string, gender: string, email: string){
 
     this.studentId = studentId
-    this.studentPwHash = studentPwHash
     this.departmentId = departmentId
     this.studentLevel = studentLevel
     this.gpa = gpa
     this.studentName = studentName
     this.ssn = ssn
-    this.birthDate = birthDate
+    this.bdate = bdate
     this.studentAddress = studentAddress
     this.phone = phone
     this.landline = landline
--- esp-src/esp-fe/src/app/services/student.service.ts
+++ esp-src/esp-fe/src/app/services/student.service.ts
@@ -40,45 +40,14 @@ export class StudentService {
       );
     }
 
-  signUp(student:Student){
-    return this._http.post<boolean>(`${this.signUpurl}`,student,{responseType:'json'});
+    catch (error) {
+      if(error instanceof HttpErrorResponse)
+        console.error('Bad request');
+
+      else
+        console.error('Error');
+    }
+    return null;
   }
 
-}
-
-export class Student{
-
-  studentId: string
-  studentPwHash: string
-  departmentId: string
-  studentLevel: string
-  gpa: string
-  studentName: string
-  ssn: string
-  birthDate: string
-  studentAddress: string
-  phone: string
-  landline: string
-  gender: string
-  email: string
-
-    constructor(studentId: string, studentPwHash: string, departmentId: string, studentLevel: string, gpa: string,
-       studentName: string, ssn: string, birthDate: string, studentAddress: string, phone: string,
-       landline: string, gender: string, email: string){
-
-        this.studentId = studentId
-        this.studentPwHash = studentPwHash
-        this.departmentId = departmentId
-        this.studentLevel = studentLevel
-        this.gpa = gpa
-        this.studentName = studentName
-        this.ssn = ssn
-        this.birthDate = birthDate
-        this.studentAddress = studentAddress
-        this.phone = phone
-        this.landline = landline
-        this.gender = gender
-        this.email = email
-
-    }
 }
