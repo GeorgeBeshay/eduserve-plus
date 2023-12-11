b9cca7a2aac0b93b50b6d478d4a601554cc310d7
David Michael <davidnagib101@gmail.com>
2023-12-11 21:40:56 +0200

    New trigger
    Added a trigger to delete student from unregistered table after being added to the registered table.

--- esp-src/esp-be/src/main/java/edu/esp/database/SQL/Triggers.sql
+++ esp-src/esp-be/src/main/java/edu/esp/database/SQL/Triggers.sql
@@ -1,13 +1,22 @@
---added a trigger so that whenever a new instructor signs up, his record gets deleted from the unregistered_instructor table
-CREATE TRIGGER DeleteCorrespondingTuple
+-- Trigger so that whenever a new instructor signs up, his record gets deleted from the unregistered_instructor table
+CREATE TRIGGER tr_instructor_remove_unregistered
 ON instructor
 AFTER INSERT
 AS
 BEGIN
     SET NOCOUNT ON;
-
-    -- Assuming that 'ID' is the common attribute between the two tables
-    -- You may need to adjust the column names based on your actual schema
     DELETE FROM unregistered_instructor
     WHERE instructor_id IN (SELECT instructor_id FROM INSERTED);
+END;
+
+
+-- Trigger so that whenever a new student signs up, his record gets deleted from the unregistered_student table
+CREATE TRIGGER tr_student_remove_unregistered
+ON student
+AFTER INSERT
+AS
+BEGIN
+    SET NOCOUNT ON;
+    DELETE FROM unregistered_student
+    WHERE student_id IN (SELECT student_id FROM INSERTED);
 END;
