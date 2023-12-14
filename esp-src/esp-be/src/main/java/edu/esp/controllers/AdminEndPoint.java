8e590df0a9b81903a1ed0f17f0beee2c3b095f4f
marshelino-maged <maromaged169@gmail.com>
2023-12-14 08:21:09 +0200

    make the APIs in the backend

--- esp-src/esp-be/src/main/java/edu/esp/controllers/AdminEndPoint.java
+++ esp-src/esp-be/src/main/java/edu/esp/controllers/AdminEndPoint.java
@@ -3,6 +3,7 @@ package edu.esp.controllers;
 import edu.esp.services.AdminServices;
 import edu.esp.system_entities.system_uni_objs.Course;
 import edu.esp.system_entities.system_users.Admin;
+import edu.esp.system_entities.system_users.Instructor;
 import edu.esp.system_entities.system_users.UnregisteredInstructor;
 import edu.esp.utilities.Hasher;
 import edu.esp.utilities.Logger;
@@ -13,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @ComponentScan(basePackages = {"edu.esp.be","edu.esp.database","edu.esp.controllers","edu.esp.system_entities"})
@@ -71,6 +73,24 @@ public class AdminEndPoint {
         return (this.adminServices.addNewCourse(newCourse))
                 ? new ResponseEntity<>(true, HttpStatus.OK)
                 : new ResponseEntity<>(false, HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("getAllCourses")
+    @ResponseBody
+    public ResponseEntity<List<Course>> getAllCourses () {
+
+        Logger.logMsgFrom(this.getClass().getName(), "An admin has requested to get all courses .. processing the request ..", -1);
+        return new ResponseEntity<>(this.adminServices.getAllCourses(), HttpStatus.OK);
+
+    }
+
+    @PostMapping("getAllUnregisteredInstructors")
+    @ResponseBody
+    public ResponseEntity<List<UnregisteredInstructor>> getAllUnregisteredInstructors () {
+
+        Logger.logMsgFrom(this.getClass().getName(), "An admin has requested to get all Unregistered Instructors .. processing the request ..", -1);
+        return new ResponseEntity<>(this.adminServices.getAllUnregisteredInstructors(), HttpStatus.OK);
+
     }
 
 }
