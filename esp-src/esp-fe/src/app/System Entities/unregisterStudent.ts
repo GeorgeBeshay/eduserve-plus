80f9f9adf067c6c05bd5459c7e69693ceda10f63
marshelino-maged <maromaged169@gmail.com>
2023-12-26 04:35:54 +0200

    make unregistered student class to make the APIs easily

---
+++ esp-src/esp-fe/src/app/System Entities/unregisterStudent.ts
@@ -0,0 +1,13 @@
+export class UnregisteredStudent{
+
+  studentId: string
+  dptId: number
+ 
+
+  constructor(studentId: string, dptId: number){
+
+    this.studentId = studentId
+    this.dptId = dptId
+
+  }
+}
