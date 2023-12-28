4b8daaa7937b10cbc26b2863820120d30cc74447
George Beshay <georgebeshay101@gmail.com>
2023-12-28 12:37:59 +0200

    Tested : - `registerCourses()`.

--- esp-src/esp-be/src/test/java/edu/esp/services/StudentServicesTests.java
+++ esp-src/esp-be/src/test/java/edu/esp/services/StudentServicesTests.java
@@ -43,7 +43,6 @@ public class StudentServicesTests {
     public void testGetCourseRegistrationSetup() {
         // Arrange
 
-
         // department
         byte dptId = 101;
         String insertingDepartmentSql = "INSERT INTO department (dpt_id) VALUES (?);";
@@ -121,6 +120,94 @@ public class StudentServicesTests {
         assertEquals(expectedCourseregistrationSetupMap, actualCourseregistrationSetupMap);
 
         // Clean
+        assertEquals(3, jdbcTemplate.update("DELETE FROM instructs WHERE instructor_id = ?;",
+                instructorId));
+        assertEquals(3, jdbcTemplate.update("DELETE FROM course WHERE course_code IN (?, ?, ?);",
+                courseCode1, courseCode2, courseCode3));
+        assertEquals(1, jdbcTemplate.update("DELETE FROM instructor WHERE instructor_id = ?", instructorId));
+        assertEquals(1, jdbcTemplate.update("DELETE FROM student WHERE student_id = ?", studentId));
+        assertEquals(1, jdbcTemplate.update("DELETE FROM department WHERE dpt_id = ?", dptId));
+    }
+
+    @Test
+    public void testRegisterCourses() {
+        // Arrange
+
+        // department
+        byte dptId = 101;
+        String insertingDepartmentSql = "INSERT INTO department (dpt_id) VALUES (?);";
+        assertEquals(1, jdbcTemplate.update(insertingDepartmentSql, dptId));
+
+        int studentId = 99;
+        Student student = new Student();
+        student.setStudentId(studentId);
+        student.setDptId(dptId);
+
+        String courseCode1 = "TEST1";
+        String courseName1 = "COURSE TEST 1";
+        String courseDescription1 = "TEST COURSE DESCRIPTION 1";
+        byte creditHrs1 = 4;
+        Course c1 = new Course(courseCode1, courseName1, courseDescription1, dptId, creditHrs1);
+
+        String courseCode2 = "TEST2";
+        String courseName2 = "COURSE TEST 2";
+        String courseDescription2 = "TEST COURSE DESCRIPTION 2";
+        byte creditHrs2 = 3;
+        Course c2 = new Course(courseCode2, courseName2, courseDescription2, dptId, creditHrs2);
+
+        String courseCode3 = "TEST3";
+        String courseName3 = "COURSE TEST 3";
+        String courseDescription3 = "TEST COURSE DESCRIPTION 3";
+        byte creditHrs3 = 3;
+        Course c3 = new Course(courseCode3, courseName3, courseDescription3, dptId, creditHrs3);
+
+        int instructorId = 99;
+        Instructor instructor = new Instructor();
+        instructor.setInstructorId(instructorId);
+        instructor.setDptId(dptId);
+
+        // needed DAOs
+        CourseDAO courseDAO = new CourseDAO(jdbcTemplate);
+        StudentDAO studentDAO = new StudentDAO(this.jdbcTemplate);
+        InstructorDAO instructorDAO = new InstructorDAO(this.jdbcTemplate);
+
+        assertTrue(courseDAO.addNewCourse(c1));
+        assertTrue(courseDAO.addNewCourse(c2));
+        assertTrue(courseDAO.addNewCourse(c3));
+        assertTrue(studentDAO.createStudent(student));
+        assertTrue(instructorDAO.createInstructor(instructor));
+
+        // instructs relation
+        byte season = 0;
+        String academicYear = "2023";
+        String instructsSql =
+                "INSERT INTO instructs (instructor_id, course_code, season, academic_year) " +
+                        "VALUES (?, ?, ?, ?);";
+
+        assertEquals(1, jdbcTemplate.update(instructsSql, instructorId, courseCode1, season, academicYear));
+        assertEquals(1, jdbcTemplate.update(instructsSql, instructorId, courseCode2, season, academicYear));
+        assertEquals(1, jdbcTemplate.update(instructsSql, instructorId, courseCode3, season, academicYear));
+
+        List<String> selectedCourses = new ArrayList<>();
+        selectedCourses.add(courseCode1);
+        selectedCourses.add(courseCode2);
+        selectedCourses.add(courseCode3);
+
+        Map<String, Object> registrationMap = new HashMap<>();
+        registrationMap.put("studentId", studentId);
+        registrationMap.put("selectedCourses", selectedCourses);
+        registrationMap.put("totalNumberOfHours", c1.getCreditHrs() + c2.getCreditHrs() + c3.getCreditHrs());
+
+        int expectedRegisteredCourses = 3;
+
+        // Act
+        int actualRegisteredCourses = studentServices.registerCourses(registrationMap);
+        assertEquals(expectedRegisteredCourses, actualRegisteredCourses);
+
+        // Assert
+
+        // Clean
+        assertEquals(3, jdbcTemplate.update("DELETE FROM grades WHERE course_code IN (?, ?, ?);", courseCode1, courseCode2, courseCode3));
         assertEquals(3, jdbcTemplate.update("DELETE FROM instructs WHERE instructor_id = ?;",
                 instructorId));
         assertEquals(3, jdbcTemplate.update("DELETE FROM course WHERE course_code IN (?, ?, ?);",
